1f110833d4c71a5e485474fad66f578b105e23e2
Aya <ayamanie7@gmail.com>
2025-11-23 02:09:52 +0200

    init commit

--- lib/core/networking/dio_endpoints.dart
+++ lib/core/networking/dio_endpoints.dart
@@ -1,5 +1,11 @@
 class ApiEndpints{
   static const BaseUrl="https://fakestoreapi.com";
   static const LoginUrl="/auth/login";
+  static const RegisterUrl="/auth/register";
+  static const products="/products";
+  static const category="/products/categories";
+  static const get_category_product="products/category/jewelery";
+  static const cart="/carts";
+
 
 }
